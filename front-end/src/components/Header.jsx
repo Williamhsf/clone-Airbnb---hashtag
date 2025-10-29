b1c63d2c60ec46d6efe247713e72c46d684eb7b5
William Henrique <williamhenriquefb@gmail.com>
2025-10-29 08:00:22 -0300

    Implement responsive layout

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="shadow-md">
+    <header className="shadow-md">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 sm:px-8">
         {/* logo */}
         <div className="flex items-center">
@@ -15,7 +15,7 @@ const Header = () => {
         </div>
 
         {/* search */}
-        <div className="flex items-center rounded-full border border-gray-300 py-2 pr-4 pl-6 shadow-md">
+        <div className="hidden lg:flex items-center rounded-full border border-gray-300 py-2 pr-4 pl-6 shadow-md">
           <p className="border-r border-r-gray-300 pr-4">Qualquer lugar</p>
           <p className="border-r border-r-gray-300 px-4">Qualquer semana</p>
           <p className="px-4">Hóspedes</p>
@@ -65,10 +65,10 @@ const Header = () => {
               clipRule="evenodd"
             />
           </svg>
-          <p>William Hen.</p>
+          <p className="sm:max-w-32 max-w-20 truncate">William Henrique</p>
         </div>
       </div>
-    </div>
+    </header>
   );
 };
 
