949c3fb291a861890b823b5903db6e224f31e469
William Henrique <williamhenriquefb@gmail.com>
2025-10-30 07:03:23 -0300

    Configure Express server and initial test route

---
+++ back-end/index.js
@@ -0,0 +1,13 @@
+import "dotenv/config"
+import express from 'express'
+
+const app = express();
+const { PORT } = process.env;
+
+app.get("/", (req, res) => {
+    res.json({ ola: "Ola Mundo!!" })
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor está rodando na porta ${PORT}`)
+})
