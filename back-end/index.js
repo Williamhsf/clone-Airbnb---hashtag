9487bc849a65b415c9afbd8da8bc1f84eca4e586
Williamhsf <williamhenriquefb@gmail.com>
2025-10-31 07:49:13 -0300

    Creates the models folder and configures the initial schema

--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,13 @@
 import "dotenv/config"
 import express from 'express'
+import { connectDb } from "./config/db";
 
 const app = express();
 const { PORT } = process.env;
 
 
 
-app.get("/", (req, res) => {
+app.get("/users", (req, res) => {
     res.json({ ola: "Ola Mundo!!" })
 })
 
