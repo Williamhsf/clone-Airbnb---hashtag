d62c55314f61fb12fff3c6faa481b70a9b6d6374
William Henrique <williamhenriquefb@gmail.com>
2025-11-01 09:12:17 -0300

    Implement user GET and POST routes with HTTP status

--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,41 @@
 import "dotenv/config"
 import express from 'express'
-import { connectDb } from "./config/db";
+import { connectDb } from "./config/db.js";
+import User from "./models/User.js";
 
 const app = express();
 const { PORT } = process.env;
 
 
 
-app.get("/users", (req, res) => {
-    res.json({ ola: "Ola Mundo!!" })
+app.get("/users", async (req, res) => {
+    connectDb()
+
+    try {
+        const userDoc = await User.find()
+
+        res.json(userDoc)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+    
+})
+
+app.post("/users", async (req, res) => {
+    connectDb()
+
+    try {
+        const newUserDoc = await User.create({
+            name: "Teste",
+            email: "Teste@teste.com",
+            password: "12345678910",
+        })
+        
+        res.json(newUserDoc)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+
 })
 
 app.listen(PORT, () => {
