23fa41b549e04f48d39369bf6bae0cf091203fcf
Williamhsf <williamhenriquefb@gmail.com>
2025-11-02 10:22:56 -0300

    Organize code and create application domains

--- back-end/index.js
+++ back-end/index.js
@@ -1,44 +1,13 @@
 import "dotenv/config"
 import express from 'express'
-import { connectDb } from "./config/db.js";
-import User from "./models/User.js";
+import UserRoutes from "./domains/users/routes.js"
 
 const app = express();
 const { PORT } = process.env;
 
+
 app.use(express.json())
-
-app.get("/users", async (req, res) => {
-    connectDb()
-
-    try {
-        const userDoc = await User.find()
-
-        res.json(userDoc)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-    
-})
-
-app.post("/users", async (req, res) => {
-    connectDb()
-
-    const { name, email, password } = req.body
-
-    try {
-        const newUserDoc = await User.create({
-            name,
-            email,
-            password,
-        })
-        
-        res.json(newUserDoc)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-
-}) 
+app.use("/users", UserRoutes)
 
 app.listen(PORT, () => {
     console.log(`Servidor está rodando na porta ${PORT}`)
