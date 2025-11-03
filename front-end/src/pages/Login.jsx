e2f0cc66ccf564ccaf51b1b21b7f4913f9c97189
William Henrique <williamhenriquefb@gmail.com>
2025-11-03 08:07:39 -0300

    Implement user GET and POST routes with HTTP status

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,16 +1,28 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
+  // let variavelNormal = "teste@gmail.com"
+  const [email, setEmail] = useState("teste@gmail.com")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    setEmail("teste2@gmail.com")
+    console.log(email)
+  }
+
   return (
     <section className='flex items-center'>
       <div className="w-full mx-auto flex max-w-96 flex-col gap-4 items-center">
         <h1 className='text-3xl font-bold'>Faça seu login</h1>
 
-        <form className='flex flex-col gap-2 w-full'>
+        <form className='flex w-full flex-col gap-2' onSubmit={handleSubmit}>
           <input 
             type="email" 
             className="w-full rounded-full border border-gray-300 px-4 py-2"
             placeholder='Digite seu e-mail'
+            value={email}
           />
           <input 
             type="password" 
