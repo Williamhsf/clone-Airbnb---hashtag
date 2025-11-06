a46b41c6b8c76c78dde13394bc3e5e5553cda4a4
William Henrique <williamhenriquefb@gmail.com>
2025-11-06 16:46:49 -0300

    complete backend refactor, JWT authentication, and full UserContext + Account Page integration

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useUserContext } from "../contexts/UserContext";
 
-const Header = ({ user }) => {
+const Header = () => {
+  const { user } = useUserContext();
   return (
     <header className="shadow-md">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 sm:px-8">
@@ -40,7 +42,7 @@ const Header = ({ user }) => {
         </Link>
 
         {/* profile */}
-        <Link to={user ? "/account" : "/login"} className="flex items-center rounded-full border border-gray-300 py-2 pr-4 pl-6 shadow-md gap-2">
+        <Link to={user ? "/account/profile" : "/login"} className="flex items-center rounded-full border border-gray-300 py-2 pr-4 pl-6 shadow-md gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
