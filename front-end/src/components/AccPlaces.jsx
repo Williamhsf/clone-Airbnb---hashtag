567f1992ee508a7c121cebd3cab29571911c981b
William Henrique <williamhenriquefb@gmail.com>
2025-11-11 17:10:33 -0300

    Style component for accommodation creation

--- front-end/src/components/AccPlaces.jsx
+++ front-end/src/components/AccPlaces.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom"
+import NewPlace from "./NewPlace";
 
 const AccPlaces = () => {
+    const { action } = useParams();
     const addNewPlace = () => {
         console.log("Adicionar novo lugar");
     }
     
     return ( 
-        <div>
-            <Link to="/account/places/new"
-                className=" hover:bg-primary-500 flex gap-2 bg-primary-400 min-w-44 cursor-pointer rounded-full px-4 py-2 text-white transition">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                </svg>
-                Adicionar novo lugar
-            </Link>
+        <div className="w-full max-w-7xl">
+            { action !== "new" ?
+                <Link to="/account/places/new"
+                    className=" hover:bg-primary-500 flex gap-2 bg-primary-400 min-w-44 cursor-pointer rounded-full px-4 py-2 text-white transition">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    Adicionar novo lugar
+                </Link>
+                : <NewPlace/>    
+            }
         </div>
     )
 }
