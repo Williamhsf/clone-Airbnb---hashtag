15c527d5b1bf2ee8f231e6ead7fc10e0ffecf83f
William Henrique <williamhenriquefb@gmail.com>
2025-11-21 07:31:03 -0300

    Adjust main container

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Item from '../components/Item'
 const Home = () => {
   return (
     <section>
-      <div className="gap-8 grid grid-cols-[repeat(auto-fit,minmax(225px,1fr))] mx-auto max-w-7xl p-8">
+      <div className="gap-8 grid grid-cols-[repeat(auto-fit,minmax(225px,1fr))] mx-auto max-w-full p-8 lg:max-w-7xl">
         <Item/>
         <Item/>
         <Item/>
