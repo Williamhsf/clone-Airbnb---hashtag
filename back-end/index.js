275132042207e59aa7a98a51d3d92d7e5bd44ff5
William Henrique <williamhenriquefb@gmail.com>
2025-11-21 07:29:09 -0300

    Create Place schema and /places route for creation

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,8 @@
 import "dotenv/config"
 import express from 'express'
 import UserRoutes from "./domains/users/routes.js"
+import PlaceRoutes from "./domains/places/routes.js"
+
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
@@ -17,6 +19,7 @@ app.use(
 )
 
 app.use("/users", UserRoutes)
+app.use("/places", PlaceRoutes)
 
 app.listen(PORT, () => {
     console.log(`Servidor está rodando na porta ${PORT}`)
