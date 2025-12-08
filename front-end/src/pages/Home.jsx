c3fb82d747da118d945cefe10ff3a2e0431a1756
William Henrique <williamhenriquefb@gmail.com>
2025-12-08 11:13:14 -0300

    Listing Accommodations on the Homepage

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,17 +1,25 @@
+import React, { useEffect, useState } from 'react';
 import Item from '../components/Item'
+import axios from 'axios';
 
 const Home = () => {
+  const [places, setPlaces] = useState([])
+    
+    useEffect(() => {
+    const axiosGet = async () => {
+      const { data } = await axios.get("/places");
+        setPlaces(data)
+    };
+
+    axiosGet();
+  }, []);
+
   return (
     <section>
       <div className="gap-8 grid grid-cols-[repeat(auto-fit,minmax(225px,1fr))] mx-auto max-w-full p-8 lg:max-w-7xl">
-        <Item/>
-        <Item/>
-        <Item/>
-        <Item/>
-        <Item/>
-        <Item/>
-        <Item/>
-        <Item/>
+        {places.map(place => (
+          <Item {...{ place }} key={place._id}/>
+        ))}
       </div>
     </section>
   )
