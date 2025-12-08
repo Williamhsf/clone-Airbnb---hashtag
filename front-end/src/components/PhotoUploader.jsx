29671853a6d6aecfc8fba5bcdcbf1d34bee442b3
William Henrique <williamhenriquefb@gmail.com>
2025-12-08 07:31:20 -0300

    Adding Buttons to Favorite and Remove Images

--- front-end/src/components/PhotoUploader.jsx
+++ front-end/src/components/PhotoUploader.jsx
@@ -7,14 +7,14 @@ const PhotoUploader = ({ photolink, setPhotoLink, setPhotos, photos }) => {
 
     if (photolink) {
       try {
-      const { data: filename } = await axios.post("/places/upload/link", {
-        link: photolink,
-      });
+        const { data: filename } = await axios.post("/places/upload/link", {
+          link: photolink,
+        });
 
-      setPhotos((prevValue) => [...prevValue, filename]);
-    } catch (error) {
-      alert("Deu erro na hora do upload por link", JSON.stringify(error));
-    }
+        setPhotos((prevValue) => [...prevValue, filename]);
+      } catch (error) {
+        alert("Deu erro na hora do upload por link", JSON.stringify(error));
+      }
     } else {
       alert("Não existe nenhum link a ser enviado!");
     }
@@ -33,7 +33,7 @@ const PhotoUploader = ({ photolink, setPhotoLink, setPhotos, photos }) => {
         headers: { "Content-Type": "multipart/form-data" },
       });
 
-      console.log(urlArray)
+      console.log(urlArray);
       setPhotos((prevValue) => [...prevValue, ...urlArray]);
     } catch (error) {
       alert("Deu erro na hora do upload", JSON.stringify(error));
@@ -65,12 +65,50 @@ const PhotoUploader = ({ photolink, setPhotoLink, setPhotos, photos }) => {
 
       <div className="mt-2 grid grid-cols-5 gap-4">
         {photos.map((photo) => (
-          <img
-            className="aspect-square rounded-2xl object-cover"
-            src={`${photo}`}
-            alt="Imagens do lugar"
-            key={photo}
-          />
+          <div className="relative">
+            <img
+              className="aspect-square rounded-2xl object-cover"
+              src={`${photo}`}
+              alt="Imagens do lugar"
+              key={photo}
+            />
+
+            <div className="absolute right-2 bottom-2 flex gap-1">
+              <div className="rounded-full bg-gray-100 p-1 opacity-75 cursor-pointer transition hover:bg-primary-400 hover:text-white">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                  />
+                </svg>
+              </div>
+
+              <div className="rounded-full bg-gray-100 p-1 opacity-75 cursor-pointer transition hover:bg-primary-400 hover:text-white">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
         ))}
 
         <label
