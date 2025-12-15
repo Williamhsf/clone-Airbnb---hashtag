92a467f15e1ec581b7b7c0ae7726080656887bf3
William Henrique <williamhenriquefb@gmail.com>
2025-12-15 11:10:17 -0300

    URL modification deploy

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -12,7 +12,7 @@ import Place from "./pages/Place";
 axios.defaults.baseURL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000/api"
-    : "http://localhost:3000/api";
+    : "https://clone-airbnb-hashtag.onrender.com/api"; //modificação de url
 
 axios.defaults.withCredentials = true;
 
