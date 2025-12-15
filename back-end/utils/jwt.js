1d3b86db75c51b17e39221ec45af7bf74b450344
William Henrique <williamhenriquefb@gmail.com>
2025-12-15 11:21:14 -0300

    modification jwt

--- back-end/utils/jwt.js
+++ back-end/utils/jwt.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import jwt from "jsonwebtoken";
 
-const { JWT_SECRET_KEY } = process.env;
+const JWT_SECRET_KEY = "lasndmabdnbf";
 
 export const JWTVerify = (req) => {
   const { token } = req.cookies;
